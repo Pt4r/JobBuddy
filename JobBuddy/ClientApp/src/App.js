1d8ee63ceab0cebb86f9b66fd9febb1a4a83df02
Pt4r <palaiologosstr@hotmail.com>
2019-12-30 23:39:11 +0200

    Add project files.

---
+++ JobBuddy/ClientApp/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+// import { Switch, Redirect } from "react-router-dom";
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
+import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
+import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
+
+import './custom.css'
+import AdminLayout from "layouts/Admin.jsx";
+
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path="/admin" render={props => <AdminLayout {...props} />} />
+        {/* <Redirect to="/admin/dashboard" /> */}
+        <Route path='/counter' component={Counter} />
+        <AuthorizeRoute path='/fetch-data' component={FetchData} />
+        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+      </Layout>
+    );
+  }
+}
---
+++ JobBuddy/ClientApp/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router} from "react-router-dom";
+import App from './App';
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/now-ui-dashboard.scss?v1.2.0";
+import "assets/css/demo.css";
+
+
+const hist = createBrowserHistory();
+
+
+ReactDOM.render(
+  <Router history={hist}>
+  <App /> 
+  </Router>,
+  document.getElementById("root")
+);
