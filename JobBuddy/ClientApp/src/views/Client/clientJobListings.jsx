43d55c996f9439577f3e5ffe892067c4fcab83f7
Stratos Paleo <palaiologosstr@hotmail.com>
2020-01-14 22:02:27 +0200

    Changes to SendGrid

--- JobBuddy/ClientApp/src/views/Client/clientJobListings.jsx
+++ JobBuddy/ClientApp/src/views/Client/clientJobListings.jsx
@@ -4,27 +4,26 @@ import React from "react";
 // core components
 import PanelHeader from "components/PanelHeader/PanelHeader.jsx";
 
-import ReactTable from "views/Tables/ReactTable";
+import ReactTables from "views/Tables/ReactTable.jsx";
+import axios from 'axios';
 
-const dataTable = [
-  ["Tiger Nixon", "System Architect", "Edinburgh", "61"],
-  ["Garrett Winters", "Accountant", "Tokyo", "63"],
-  ["Ashton Cox", "Junior Technical Author", "San Francisco", "66"],
-  ["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "22"],
-  ["Airi Satou", "Accountant", "Tokyo", "33"],
-  ["Brielle Williamson", "Integration Specialist", "New York", "61"],
-  ["Herrod Chandler", "Sales Assistant", "San Francisco", "59"],
-  ["Rhona Davidson", "Integration Specialist", "Tokyo", "55"],
-  ["Colleen Hurst", "Javascript Developer", "San Francisco", "39"],
-  ["Sonya Frost", "Software Engineer", "Edinburgh", "23"],
-  ["Jena Gaines", "Office Manager", "London", "30"],
-  ["Quinn Flynn", "Support Lead", "Edinburgh", "22"],
-  ["Charde Marshall", "Regional Director", "San Francisco", "36"],
-  ["Haley Kennedy", "Senior Marketing Designer", "London", "43"],
-  ["Tatyana Fitzpatrick", "Regional Director", "London", "19"],
-  ["Michael Silva", "Marketing Designer", "London", "66"],
-  ["Paul Byrd", "Chief Financial Officer (CFO)", "New York", "64"]
-];
+const dataTable = ()=>{
+
+  axios.post("https://jsonplaceholder.typicode.com/users").then(function(response){console.log(response)});
+
+
+  var result;
+  const url = "https://jsonplaceholder.typicode.com/users";
+  fetch(url, {
+    method: "GET"
+  }).then(res => res.json())
+  .then(data => {
+    result=data;
+  })
+  console.log(result);
+
+  return result;
+};
 
 var columns =[
   ["Name","name"],
@@ -34,6 +33,8 @@ var columns =[
   ["Actions","actions", false, false]
 ];
 
+
+
 class clientJobListings extends React.Component {
   constructor(props) {
     super(props);
@@ -42,11 +43,15 @@ class clientJobListings extends React.Component {
       data: dataTable
     };
   }
+componentDidMount(){
+  axios.post("https://jsonplaceholder.typicode.com/users").then(function(response){console.log(response)});
+}
+  
     render() {
       return (
         <>
         <PanelHeader size="sm" />
-        <ReactTable
+        <ReactTables
         title="Job Listings"
         columns={this.state.columns}
         data={this.state.data}
